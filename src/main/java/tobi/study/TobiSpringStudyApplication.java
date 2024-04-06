61807c1d9447522df1dff4c3dc7fdefd26a66fa6
hmmini <pak4184@naver.com>
2024-04-06 22:53:23 +0900

    1.1 초난감 DAO

---
+++ src/main/java/tobi/study/TobiSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package tobi.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiSpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobiSpringStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tobi/study/TobiSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package tobi.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiSpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
