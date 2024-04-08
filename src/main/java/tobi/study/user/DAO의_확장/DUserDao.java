e8dde5db47bfd54241653b69068038f851f46a82
hmmini <pak4184@naver.com>
2024-04-08 17:24:04 +0900

    DAO의 확장

---
+++ src/main/java/tobi/study/user/DAO의_확장/DUserDao.java
@@ -0,0 +1,17 @@
+package tobi.study.user.DAO의_확장;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao {
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        // D사 DB Connection 생성 코드
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/tobiSpringStudy", "sa", ""
+        );
+        return c;
+    }
+}
