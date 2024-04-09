99f41f0653b1fb228f54630355f26aefb27d4a58
hmmini <pak4184@naver.com>
2024-04-10 00:44:46 +0900

    DAO의 확장 - 클래스분리

---
+++ src/main/java/tobi/study/user/DAO의_확장_1_3/클래스분리/SimpleConnectionMaker.java
@@ -0,0 +1,15 @@
+package tobi.study.user.DAO의_확장_1_3.클래스분리;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/tobiSpringStudy", "sa", ""
+        );
+        return c;
+    }
+}
