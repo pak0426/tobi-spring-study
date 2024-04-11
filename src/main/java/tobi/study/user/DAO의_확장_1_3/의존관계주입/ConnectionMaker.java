5c45c4f2dd7a4b4ac49e1fbac669676a28573acd
hmmini <pak4184@naver.com>
2024-04-12 00:47:05 +0900

    DAO의 확장 - 의존관계주입

---
+++ src/main/java/tobi/study/user/DAO의_확장_1_3/의존관계주입/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package tobi.study.user.DAO의_확장_1_3.의존관계주입;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException;
+}
