505528a02290316b39d44e559fee3ed1d017515f
hmmini <pak4184@naver.com>
2024-04-14 19:18:47 +0900

    fix

--- src/main/java/tobi/study/user/DAO의_분리_1_2/DUserDao.java
+++ src/main/java/tobi/study/user/DAO의_분리_1_2/DUserDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DUserDao extends UserDao {
+class DUserDao extends UserDao {
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         // D사 DB Connection 생성 코드
--- src/main/java/tobi/study/user/DAO의_분리_1_2/NUserDao.java
+++ src/main/java/tobi/study/user/DAO의_분리_1_2/NUserDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class NUserDao extends UserDao {
+class NUserDao extends UserDao {
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         // N사 DB Connection 생성 코드
