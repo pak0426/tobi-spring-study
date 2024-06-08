e63c4ad1341f372b74a52ae72f71b85bf1f141ab
hmmini <pak4184@naver.com>
2024-06-08 12:00:00 +0900

    5.2 트랜잭션 서비스 추상화

---
+++ src/main/java/tobi/study/user/STEP5/트랜잭션_서비스_추상화_5_2/DaoFactory.java
@@ -0,0 +1,36 @@
+package tobi.study.user.STEP5.트랜잭션_서비스_추상화_5_2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+class DaoFactory {
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUrl("jdbc:h2:tcp://localhost/~/tobiSpringStudy");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    public UserDao userDao() {
+        UserDaoJdbc userDaoJdbc = new UserDaoJdbc();
+        userDaoJdbc.setDataSource(dataSource());
+        return userDaoJdbc;
+    }
+
+    @Bean
+    public UserService userService() {
+        UserService userService = new UserService();
+        userService.setUserDao(userDao());
+        return userService;
+    }
+}
---
+++ src/main/java/tobi/study/user/STEP5/트랜잭션_서비스_추상화_5_2/UserDaoJdbc.java
@@ -0,0 +1,65 @@
+package tobi.study.user.STEP5.트랜잭션_서비스_추상화_5_2;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class UserDaoJdbc implements UserDao {
+    private JdbcTemplate jdbcTemplate;
+
+
+    private RowMapper<User> userMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        user.setLevel(Level.valueOf(rs.getInt("level")));
+        user.setLogin(rs.getInt("login"));
+        user.setRecommend(rs.getInt("recommend"));
+        return user;
+    };
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public DataSource getDataSource() {
+        return jdbcTemplate.getDataSource();
+    }
+
+    @Override
+    public void add(final User user) {
+        jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) values (?, ?, ?, ?, ?, ?)", user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
+    }
+
+    @Override
+    public User get(String id) {
+        return jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, userMapper);
+    }
+
+    @Override
+    public void deleteAll() {
+        jdbcTemplate.update("delete from users");
+    }
+
+    @Override
+    public int getCount() {
+        return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return jdbcTemplate.query("select * from users", userMapper);
+    }
+
+    @Override
+    public void update(User user) {
+        jdbcTemplate.update(
+                "update users set name = ?, password = ?, LEVEL = ?, LOGIN = ?, RECOMMEND = ? where ID = ?",
+                user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(),
+                user.getId()
+        );
+    }
+}
